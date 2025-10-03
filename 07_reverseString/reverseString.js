223a2deb9f337c553369e916625eabcbd767eed2
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2025-10-03 14:58:48 -0400

    finished js exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+/* 
+- break string into individual letters in array
+- loop through array in reverse order
+- write letters to output
+*/
+    let string = input
+    let array = []
+    for (i = 0; i < input.length; i++) {
+        array.push(string[i])
+    }
+    array.reverse()
+    return array.join("")
 };
 
 // Do not edit below this line
