efac090767373d7608994801c4665f0c7e385fa3
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2025-10-03 14:26:48 -0400

    solves js exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string = ""
+    for (i = 0; i < num; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
