2fd32f4117a1a476a612aaf802923f9d688d4cdd
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2025-10-05 14:38:42 -0400

    js exercise 10 solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+/*
+leap years are:
+- divisible by 4
+- divisible by 400
+- not divisible by 100
+*/
+isLeapYear = false
 
+if (year % 100 === 0) {
+    if (year % 400 === 0) {
+        isLeapYear = true;
+    };
+} else {
+    if (year % 4 === 0) {
+        isLeapYear = true;
+    };
 };
-
+return isLeapYear;
+};
 // Do not edit below this line
 module.exports = leapYears;
