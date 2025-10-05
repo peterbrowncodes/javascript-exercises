ebb7529427ee5768a6e0542b82f5610a6623a0f0
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2025-10-05 14:19:21 -0400

    js exercise 09 solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+/*
+- check the parameters are both positive
+- variable to store the final sum
+- loop through all numbers between the parameters
+- add current number to the final sum
+- iterate up 1 number through the final number
+*/
+    if ((a > 0 && Number.isInteger(a)) && (b > 0 && Number.isInteger(b))){
+        let finalSum = 0
+        if (a > b) {
+            for (i = a; i >= b; i--) {
+            finalSum += i;
+            }
+        } else {
+            for (i = a; i <= b; i++) {
+            finalSum += i;    
+            }
+        };
+        return finalSum;
+    } else {
+        return "ERROR"
+    };
 };
 
 // Do not edit below this line
