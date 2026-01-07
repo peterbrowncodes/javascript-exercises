89cebe88f6e81f224f731056ff058066a86b501a
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-06 21:43:29 -0500

    solution exercise 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objs) {
+  const newArray = objs.map((obj) => obj.title);
+  return newArray;
 };
 
 // Do not edit below this line
