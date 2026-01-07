80d1a325711f034dd4feac9ce7aa409fcb33fa2d
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-06 21:35:55 -0500

    fibonacci sucks

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let firstPrev = 1;
+  let secondPrev = 0;
+  let count 
 
+  if (typeof(n) !== 'number') {
+    count = parseInt(n);
+  } else {
+    count = n;
+  }
+
+  if (count < 0) {
+    return 'OOPS';
+  };
+  if (count === 0){
+    return 0;
+  }
+
+  for (i = 2; i <= count; i++) {
+    current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+  return firstPrev;
 };
 
 // Do not edit below this line
