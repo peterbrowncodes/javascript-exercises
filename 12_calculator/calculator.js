ae6fc304082e2fe4f4655b7fc5c30cc5128dd639
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-06 20:03:36 -0500

    solve exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	const total = args.reduce((accumulator, currentElement) =>
+    accumulator + currentElement, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((acc, current) => acc * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = 1;
+  for (i = 0; i < b; i++) {
+    total *= a
+  };
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (i = 1; i <= a; i++) {
+    total *= i;
+  };
+  return total;
 };
 
 // Do not edit below this line
