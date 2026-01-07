ba7a68a9a90f7393f6149023ffacc210378fd699
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-06 20:43:29 -0500

    solutions for exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const filteredString = string
+    .toLowerCase()
+    .split('')
+    .filter((a) => alphanumerical.includes(a))
+    .join('')
+  const reverseString = filteredString.split('').reverse().join('');
+
+  if (filteredString === reverseString){
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
